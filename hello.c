e4d28c2d841e4c9ed76ade9b6cba406fec05449f
Yousif Alboufradi <yalboufradi2@huskers.unl.edu>
2025-08-28 09:50:45 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,21 @@
+/**
+ * Author: Yousif Alboufradi
+ *
+ * Date: 2025-08-28
+ *
+ * A simple program that prints my name
+ *
+ */
+
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Yousif Alboufradi\n");
+
+    return 0;
+}
+
