45cfa99afb5071ed189f1e0280f87311c3f1d7e5
Yousif Alboufradi <yalboufradi2@huskers.unl.edu>
2025-09-12 09:51:25 +0000

    Fri, Sep 12, 2025, 4:51 AM -05:00

--- dataPlan.c
+++ dataPlan.c
@@ -51,7 +51,7 @@ int main(int argc, char **argv) {
     remainingGbs = totalGbs - numGbs;
 
     // remaining days
-    reaminingDays = 30 - day;
+    remainingDays = 30 - day;
 
     // how much gbs can be used per day
     gbPerDay = remainingGbs / remainingDays;
@@ -60,10 +60,10 @@ int main(int argc, char **argv) {
     if(avgUsage < baseAverage) {
         // if the user is under their average daily use
         printf("%d days used, %d days remaining\n Average daily use: %fGB/day\n", day, remainingDays, avgUsage);
-        printf("\nYou are at or below your average daily use (%fGB/day)\n You can use up to %fGB/day and stay below your data plan limit\n", avgUsage, gbPerDay);
+        printf("\nYou are at or below your average daily use (%fGB/day)\n You can use up to %fGB/day and stay below your data plan limit\n", baseAverage, gbPerDay);
     } else if(avgUsage > baseAverage) {
         // if the user is over their average daily use
-        printf("\n%ddays used, %ddays remaining\n Average daily use: %fGB/day\n" );
+        printf("\n%ddays used, %ddays remaining\n Average daily use: %fGB/day\n", day, remainingDays, avgUsage);
         printf("You have already met your limit for this month. Looks like you're getting some overage charges...\n");
     }
 
