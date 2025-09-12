48cdfcc7bca381231dd73a5ffad2d3ac4f296984
Yousif Alboufradi <yalboufradi2@huskers.unl.edu>
2025-09-12 09:13:02 +0000

    Fri, Sep 12, 2025, 4:13 AM -05:00

--- dataPlan.c
+++ dataPlan.c
@@ -29,6 +29,11 @@ int main(int argc, char **argv) {
     double numGbs = atof(argv[1]);
     int day = atoi(argv[2]);
     double totalGbs = atof(argv[3]);
+    double avgUsage;
+    double baseAverage;
+    double remainingGbs;
+    int remainingDays;
+    double gbPerDay;
 
 
 
@@ -37,18 +42,36 @@ int main(int argc, char **argv) {
 
 
     // computing average usage
-    avgUsage = totalGbs / day // ex: if we have 25gb total used / the 5th day then we get a 5gb/day usage
+    avgUsage = numGbs / day; // ex: if we have 25gb used / the 5th day then we get a 5gb/day usage
+
+    // average expected usage
+    baseAverage = numGbs / 30;
+
+    // how much gbs left
+    remainingGbs = totalGbs - numGbs;
+
+    // how much gbs can be used per day
+    gbPerDay = remainingGbs / remainingDays;
+
+
+    if(avgUsage < baseAverage) {
+        // if the user is under their average daily use
+        printf("%d days used, %d days remaining\n Average daily use: %fGB/day\n", day, remainingDays, avgUsage);
+        printf("\nYou are at or below your average daily use (%fGB/day)\n You can use up to %fGB/day and stay below your data plan limit\n", avgUsage, gbPerDay);
+    } else if(avgUsage > baseAverage) {
+        // if the user is over their average daily use
+        printf("\ndays used,  days remaining\n Average daily use: GB/day\n" );
+        printf("You have already met your limit for this month. Looks like you're getting some overage charges...\n");
+    }
 
 
 
-    // if the user is under their average daily use
-    printf(" days used,  days remaining\n Average daily use: GB/day\n" );
-    printf("\nYou are at or below your average daily use (GB/day)\n You can use up to GB/day and stay below your data plan limit\n");
 
 
-    // if the user is over their average daily use
-    printf("\ndays used,  days remaining\n Average daily use: GB/day\n" );
-    printf("You have already met your limit for this month. Looks like you're getting some overage charges...\n");
+
+
+
+
 
 
     return 0;
