19014c11327e5a0058f9dacb7859fddd0c38cee7
Mason <mm766317@southeast.edu>
2025-09-18 10:27:34 -0500

    Update retire.c

--- retire.c
+++ retire.c
@@ -15,61 +15,40 @@
  #include <math.h>
  #include <stdlib.h>
 
+#include <stdio.h>
+#include <stdlib.h>
 
- int main(int argc, char **argv) {
-
-
-    double startingBalance = atof(argv[1]);
+int main(int argc, char **argv) {
+    double startBalance = atof(argv[1]);
     double monthlyCont = atof(argv[2]);
     double averageReturn = atof(argv[3]);
     double averageInflation = atof(argv[4]);
     double retirement = atof(argv[5]);
-
     int years;
 
-    double adjustedRate = ((1 + averageReturn) / (1 + averageInflation)) - 1; // inflation adjusted rate of return formula
+    double adjustedRate = ((1 + averageReturn) / (1 + averageInflation)) - 1;
 
-    double monthlyRate = (averageReturn / 12.0);
+    double monthlyRate = (averageReturn / 100) / 12.0;
 
-    double balance = startingBalance;
+    double balance = startBalance;
     double totalInterest = 0.0;
-    int months =  years * 12;
+    int months = years * 12;
 
-    for(int i=1; i<= months; i++) {
-      double baseForInterest = balance + monthlyCont;
-      double interest = baseForInterest * monthlyRate;
-
-      balance = baseForInterest + interest;
-      totalInterest += interest; 
-    }
-  printf("Starting balance: ");
-    if (scanf("%lf", &startBalance) != 1) {
-        printf("ERROR\n"); return 1;
-    }
-
-    printf("Monthly contribution: ");
-    if (scanf("%lf", &monthlyCont) != 1) {
-        printf("ERROR\n"); return 1;
-    }
-
-    printf("Expected annual return (percent): ");
-    if (scanf("%lf", &averageReturn) != 1) {
-        printf("ERROR\n"); return 1;
-    }
-    printf("Expected inflation (percent): ");
-    if (scanf("%lf", &averageInflation) != 1) {
-        printf("ERROR\n"); return 1;
-    }
-
-    printf("Years to project: ");
-    if (scanf("%d", &years) != 1 || years <= 0) {
-        printf("ERROR\n"); return 1;
-    }
     printf("Month    Interest     Balance\n");
+
+    for (int i = 1; i <= months; i++) {
+        double baseForInterest = balance + monthlyCont;
+        double interest = baseForInterest * monthlyRate;
+
+        balance = baseForInterest + interest;
+        totalInterest += interest;
 
         printf("%5d $%10.2f $%10.2f\n", i, interest, balance);
     }
 
     printf("Total Interest Earned: $%10.2f\n", totalInterest);
     printf("Total Nest Egg: $%10.2f\n", balance);
- }
+
+    return 0;
+}
+
