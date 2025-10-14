039bbb5c447e8a8499a92493312c49f68445d5fd
Yousif <145300688+Yousif09@users.noreply.github.com>
2025-10-14 09:43:11 -0500

    Update colorUtils (1).h

--- colorUtils (1).h
+++ colorUtils (1).h
@@ -1,15 +1,47 @@
-#ifndef COLOR_UTILS_H
-#define COLOR_UTILS_H
+/**
+ *
+ * Yousif Alboufradi
+ * Mason Melcher
+ *
+ * 2025-10-09
+ *
+ * Header file
+ *
+ */
 
-typedef enum {
-  colorOk        = 0,
-  colorNull      = 1 << 0,
-  colorRange     = 1 << 1,
-  colorNotFinite = 1 << 2
-} ColorError;
 
+ typedef enum {
+    COLOR_OK,
+    COLOR_NULL,
+    COLOR_RANGE
+ } ColorError;
+
+
+
+
+/**
+ *
+ * returns the largest of the three values
+ *
+ *
+ */
+double maxOfThree(double a, double b, double c);
+
+
+
+/**
+ *
+ * Converts an RGB color model to CMYK
+ *
+ */
 int rgbToCMYK(int r, int g, int b, double *c, double *m, double *y, double *k);
 
+
+
+/**
+ *
+ * Converts a CMYK color model to RGB
+ *
+ */
 int cmykToRGB(double c, double m, double y, double k, int *r, int *g, int *b);
 
-#endif
